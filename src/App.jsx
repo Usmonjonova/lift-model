7d0c1a6a751a36bc18b5dd34eaa692286a71ae4e
Ziyoda Usmonjonova <ziyodausmonjonova1@gmail.com>
2024-11-14 19:17:50 +0500

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="lift-control-wrapper">
+      <div class="lift-wrapper">
+        <div class="lift">
+          <div class="lift-item">1</div>
+        </div>
+        <div class="lift">
+          <div class="lift-item">1</div>
+        </div>
+        <div class="lift">
+          <div class="lift-item">1</div>
+        </div>
+      </div>
+      <div class="control-wrapper">
+        <div class="lift-floor-number">
+          <div class="floor-number-wrap">
+            <div class="floor-number">1</div>
+            <div class="arrow-up">
+              <img src="./images/arrow-up.svg" alt="arrow-up" />
+            </div>
+            <div class="arrow-down">
+              <img src="./images/arrow-down.svg" alt="arrow-down" />
+            </div>
+          </div>
+          <div class="floor-number-wrap">
+            <div class="floor-number">2</div>
+            <div class="arrow-up">
+              <img src="./images/arrow-up.svg" alt="arrow-up" />
+            </div>
+            <div class="arrow-down">
+              <img src="./images/arrow-down.svg" alt="arrow-down" />
+            </div>
+          </div>
+          <div class="floor-number-wrap">
+            <div class="floor-number">3</div>
+            <div class="arrow-up">
+              <img src="./images/arrow-up.svg" alt="arrow-up" />
+            </div>
+            <div class="arrow-down">
+              <img src="./images/arrow-down.svg" alt="arrow-down" />
+            </div>
+          </div>
+          <div class="floor-number-wrap">
+            <div class="floor-number">4</div>
+            <div class="arrow-up">
+              <img src="./images/arrow-up.svg" alt="arrow-up" />
+            </div>
+            <div class="arrow-down">
+              <img src="./images/arrow-down.svg" alt="arrow-down" />
+            </div>
+          </div>
+          <div class="floor-number-wrap">
+            <div class="floor-number">5</div>
+            <div class="arrow-up">
+              <img src="./images/arrow-up.svg" alt="arrow-up" />
+            </div>
+            <div class="arrow-down">
+              <img src="./images/arrow-down.svg" alt="arrow-down" />
+            </div>
+          </div>
+          <div class="floor-number-wrap">
+            <div class="floor-number">6</div>
+            <div class="arrow-up">
+              <img src="./images/arrow-up.svg" alt="arrow-up" />
+            </div>
+            <div class="arrow-down">
+              <img src="./images/arrow-down.svg" alt="arrow-down" />
+            </div>
+          </div>
+          <div class="floor-number-wrap">
+            <div class="floor-number">7</div>
+            <div class="arrow-up">
+              <img src="./images/arrow-up.svg" alt="arrow-up" />
+            </div>
+            <div class="arrow-down">
+              <img src="./images/arrow-down.svg" alt="arrow-down" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
