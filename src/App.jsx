2f3224784885b8282b6934b14a742f2a91212a60
Ziyoda Usmonjonova <ziyodausmonjonova1@gmail.com>
2024-11-15 22:44:54 +0500

    rewrite code & some corrections in css

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
-import { useState } from 'react';
-import './App.css';
-import arrowUp from './images/arrow-down.svg';
-import arrowDown from './images/arrow-up.svg';
-import styled, { keyframes } from 'styled-components';
+import React, { useEffect, useState } from "react";
+import arrowUp from "./images/arrow-up.svg";
+import arrowDown from "./images/arrow-down.svg";
+import "./App.css";
 
-const createKeyframes = (steps, upDown) => {
-  const keyframesArray = [];
-  const stepPercentage = 100 / (steps - 1);
-
-  for (let i = 0; i < steps; i++) {
-    keyframesArray.push(`
-        ${i * stepPercentage}% { transform: translateY(${-77 * i}px); }
-      `);
-  }
-  return keyframes`${keyframesArray.join(" ")}`;
-};
-
-const LiftItem = styled.div`
-    animation: ${({ steps }) => createKeyframes(steps)} 3s forwards;
-  `;
-
-
-function App() {
-  const [clickedFloorNumber, setClickedFloorNumber] = useState(1);
-  const [upFloor, setUpFloor] = useState(true)
+const App = () => {
+  const [liftModel, setLiftModel] = useState([
+    {
+      isItWorking: true,
+      prevFloor: 1,
+      currentFloor: 1,
+      nextFloor: 1,
+      upFloor: false,
+      downFloor: false,
+    },
+  ]);
 
   const floors = [
     { id: 7, floorNumber: 7 },
@@ -32,50 +22,86 @@ function App() {
     { id: 4, floorNumber: 4 },
     { id: 3, floorNumber: 3 },
     { id: 2, floorNumber: 2 },
-    { id: 1, floorNumber: 1 }
+    { id: 1, floorNumber: 1 },
   ];
 
-  function handleFloorUp(floorNumber) {
-    setClickedFloorNumber(floorNumber);
-    setUpFloor(true)
-  }
+  useEffect(() => {
+    if (liftModel[0].nextFloor !== liftModel[0].prevFloor) {
 
-  function handleFloorDown(floorNumber) {
-    setClickedFloorNumber(floorNumber);
-    setUpFloor(false)
-  }
+      createLiftAnimation(liftModel[0].prevFloor, liftModel[0].nextFloor);
+    }
+  }, [liftModel]);
+
+  const createLiftAnimation = (prevFloor, nextFloor) => {
+    const startY = -(prevFloor - 1) * 77;
+    const endY = -(nextFloor - 1) * 77;
+
+    const styleSheet = document.styleSheets[0];
+    const animationName = "lift-move";
+    const keyframes = `
+      @keyframes ${animationName} {
+        0% { transform: translateY(${startY}px); }
+        100% { transform: translateY(${endY}px); }
+      }
+    `;
+
+    for (let i = 0; i < styleSheet.cssRules.length; i++) {
+
+      if (styleSheet.cssRules[i].name === animationName) {
+
+        console.log(styleSheet.cssRules[i]);
+        styleSheet.deleteRule(i);
+        break;
+      }
+    }
+
+    styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
+  };
+
+  const moveLiftTo = (floorNumber) => {
+    setLiftModel((prevModel) => {
+      const updatedModel = {
+        ...prevModel[0],
+        prevFloor: prevModel[0].currentFloor,
+        currentFloor: floorNumber,
+        nextFloor: floorNumber,
+        upFloor: floorNumber > prevModel[0].currentFloor,
+        downFloor: floorNumber < prevModel[0].currentFloor,
+      };
+      return [updatedModel];
+    });
+  };
 
   return (
     <div className="lift-control-wrapper">
       <div className="lift-wrapper">
         <div className="lift">
-          <LiftItem steps={clickedFloorNumber} className="lift-item">{clickedFloorNumber}</LiftItem>
+          <div
+            className="lift-item">{liftModel[0].currentFloor}</div>
         </div>
       </div>
+
       <div className="control-wrapper">
         <div className="lift-floor-number">
-          {
-            floors.map(floor => (
-              <div key={floor.id} className="floor-number-wrap">
-                <div className="floor-number">{floor.floorNumber}</div>
-
-                {floor.id !== 7 && (
-                  <div onClick={() => handleFloorDown(floor.id)} className="arrow-down">
-                    <img src={arrowDown} alt="arrow-down" />
-                  </div>
-                )}
-                {floor.id !== 1 && (
-                  <div onClick={() => handleFloorUp(floor.id)} className="arrow-up">
-                    <img src={arrowUp} alt="arrow-up" />
-                  </div>
-                )}
-              </div>
-            ))
-          }
+          {floors.map((floor) => (
+            <div key={floor.id} className="floor-number-wrap">
+              <div className="floor-number">{floor.floorNumber}</div>
+              {floor.id !== 1 && (
+                <div onClick={() => moveLiftTo(floor.floorNumber)} className="arrow-down">
+                  <img src={arrowDown} alt="Move Down" aria-label="Move to a lower floor" />
+                </div>
+              )}
+              {floor.id !== 7 && (
+                <div onClick={() => moveLiftTo(floor.floorNumber)} className="arrow-up">
+                  <img src={arrowUp} alt="Move Up" aria-label="Move to a higher floor" />
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
